eebfceb960edc54b6440a4e4cd5166369f3802e8
Ryan <47619352+rpt24@users.noreply.github.com>
2025-07-29 16:47:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,6 @@ def api_leaderboard():
     return jsonify(sorted_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
